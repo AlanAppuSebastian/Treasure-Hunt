b121f5384839cc9c9f7bc480f13c1d7cdc1dfc37
Alan Sebastian <68325551+AlanAppuSebastian@users.noreply.github.com>
2021-05-06 16:06:11 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "DATABASE";
+const accessCode3 = "FUNCTION";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+fill("white")
+textSize(15)
+text("A T A D E S B A", 708,158)
+fill("lightblue")
+text("Hint:It stores information  !!", 700,180)
+
+fill("white")
+textSize(15)
+text("C U T N I F O N", 115,380)
+fill("lightblue")
+text("Hint:It helps to do a task  !!", 115,403  );
+
+
+}
